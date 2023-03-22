c9090bfa20aca26133da7cb1a6e148f2fab780ff
Migggzz <miggz.gommz@gmail.com>
2023-03-22 12:29:04 -0600

    added next and prev links on view page

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -7,11 +7,14 @@ router.get('/',(req,res)=> res.render('index'));
 
 router.get('/users', async (req,res)=>{
     let page = parseInt(req.query.page);
+    let limit = parseInt(req.query.limit);
     if(!page) page =1;
-        const result = await UserModel.paginate({},{page,limit:5, lean: true});
-        result.prevLink = result.hasPrevPage ? `/users?page=${result.prevPage}` : '';
-        result.nextLink = result.hasNextPage ? `/users?page=${result.nextPage}` : '';
-        res.render('users',result);
+    if(!limit) limit = 5;
+    const result = await UserModel.paginate({},{page,limit, lean: true});
+    result.prevLink = result.hasPrevPage ? `/users?page=${result.prevPage}&limit=${limit}` : '';
+    result.nextLink = result.hasNextPage ? `/users?page=${result.nextPage}&limit=${limit}` : '';
+    console.log(result)
+    res.render('users',result);
 })
 
 
